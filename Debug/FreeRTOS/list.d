67f0344d98b1d29365f80fa6a5982ee6df7bfc96
chieyu <chienyu684@gmail.com>
2022-03-14 18:42:14 +0800

    first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,25 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/projdefs.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/portable.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/list.h
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/projdefs.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/portable.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/chien/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3_NE6091116/FreeRTOS/include/list.h:
---
+++ README.md
@@ -0,0 +1,20 @@
+"# lab3-tsek_monitor-chien-yu-hsu" 
+# Embedded_OS_Lab3_NE6091116
+## Overview
+**step1.**
+Create four tasks
+
+Red LED blink. priority = 1，
+Green LED blink. priority = 1，
+TaskMonitor. priority = 2，
+Delay Task.  priority = 14。
+
+**step2.**
+Task Monitor periodicity execute the Taskmonitor API.
+
+## TaskMonitor_App
+
+依照投影片的說明，分別印出Name、Priority、pxStack......
+
+![lab3 print](https://user-images.githubusercontent.com/80887185/124144988-04daf800-dabf-11eb-8840-9a3716a3e91c.png)
+"# FreeRTOS_Lab3" 
